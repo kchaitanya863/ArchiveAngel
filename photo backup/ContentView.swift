16f4e1adae5fb99f274fd0615cd73d15aa42ea44
Krishna Chaitanya <kchaitanya863@gmail.com>
2023-11-13 17:20:18 -0800

    Initial Commit

---
+++ photo backup/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  photo backup
+//
+//  Created by Krishna on 11/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ photo backup/photo_backupApp.swift
@@ -0,0 +1,17 @@
+//
+//  photo_backupApp.swift
+//  photo backup
+//
+//  Created by Krishna on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct photo_backupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
